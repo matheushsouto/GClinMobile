a1e07871229ccd9b1ad63ef8696e79ef1a36bbf5
Matheus Henrique Souto <matheushsouto@gmail.com>
2019-09-13 22:43:18 -0300

    Add page evolucao paciente

---
+++ src/pages/evolucaopaciente/evolucaopaciente.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { EvolucaopacientePage } from './evolucaopaciente';
+
+@NgModule({
+  declarations: [
+    EvolucaopacientePage,
+  ],
+  imports: [
+    IonicPageModule.forChild(EvolucaopacientePage),
+  ],
+})
+export class EvolucaopacientePageModule {}
---
+++ src/pages/evolucaopaciente/evolucaopaciente.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
+
+/**
+ * Generated class for the EvolucaopacientePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage({
+  name: 'evolucao-page'
+})
+@Component({
+  selector: 'page-evolucaopaciente',
+  templateUrl: 'evolucaopaciente.html',
+})
+export class EvolucaopacientePage {
+  pacientesDb;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public http: Http,
+  ) {
+    this.pacientesDb = [];
+  }
+
+  ionViewDidLoad() {
+    this.pegarDadosFirebase();
+  }
+
+  pegarDadosFirebase() {
+    this.http.get('https://fir-login-8f2fd.firebaseio.com/paciente')
+      .map(res => res.json())
+      .subscribe(data => {
+        if (data !== null && data !== undefined) {
+          this.trataDados(data);
+          console.log(data, 'dados do firebase');
+        }
+      })
+  }
+  trataDados(dados) {
+    this.pacientesDb = Object.keys(dados).map(i => dados[i]);
+  }
+
+}
